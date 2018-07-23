4bc1487a1c9d6ade6a2733d2c476f25c119da445
Kael Shipman <kael.shipman@gmail.com>
2018-07-22 22:59:31 -0500

    Improved build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,16 +7,7 @@ set -e
 if [ -z "$KSSTDLIBS_PATH" ]; then 
     KSSTDLIBS_PATH=/usr/lib/ks-std-libs
 fi
-if [ ! -e "$KSSTDLIBS_PATH/libpkgbuilder.sh" ]; then
-    >&2 echo
-    >&2 echo "E: Your system doesn't appear to have ks-std-libs installed. (Looking for"
-    >&2 echo "   library 'libpkgbuilder.sh' in $KSSTDLIBS_PATH. To define a different"
-    >&2 echo "   place to look for this file, just export the 'KSSTDLIBS_PATH' environment"
-    >&2 echo "   variable.)"
-    >&2 echo
-    exit 4
-fi
-. "$KSSTDLIBS_PATH/libpkgbuilder.sh"
+. require libpkgbuilder.sh "$KSSTDLIBS_PATH"
 
 function build() {
     local pkg="$1"
@@ -32,11 +23,14 @@ function build() {
 
     sudo cp -a "$pkg" build/
     build_deb_package "$pkg" "build"
-    rm -R build/"$(basename "$pkg")"
+    sudo rm -Rf "build/$(basename "$pkg")"
 }
 
-if [ -n "$1" ]; then
-    build "$1"
+if [ "$#" -gt 0 ]; then
+    while [ "$#" -gt 0 ]; do
+        build "$1"
+        shift
+    done
 else
     for p in dev/*; do
         if [ ! -d "$p" ] || [ "$(basename "$p")" == "pkg-skel" ]; then
